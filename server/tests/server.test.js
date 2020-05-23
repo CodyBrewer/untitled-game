6b04bb51695912a851ad761e27537755b41219ca
CodyBrewer <codybrewerdev@gmail.com>
2020-05-23 16:09:54 -0600

    server test written and passing

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,7 +1,16 @@
 const { graphql } = require('graphql');
 
-const server = require('../server.js');
+const { schema } = require('../server.js');
+
+const infoQuery = `
+  query{
+    info
+  }
+`
 
 describe('GraphQL API', () => {
-  it.todo(`Returns { "data": { "info": "This is the API for Cody's Untitled Game." } }`);
+  it(`infoQuery returns { "data": { "info": "This is the API for Cody's Untitled Game." } }`, async () =>{
+    const response = await graphql(schema, infoQuery, null, {}, {})
+    expect(response.data.info).toBe("This is the API for Cody's Untitled Game.");
+  });
 });
