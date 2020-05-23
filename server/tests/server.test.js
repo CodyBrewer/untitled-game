23a6bcd81adbd53509a14637a5221284b0e0320e
CodyBrewer <codybrewerdev@gmail.com>
2020-05-23 15:59:32 -0600

    add graphql to deps

---
+++ server/tests/server.test.js
@@ -0,0 +1,7 @@
+const { graphql } = require('graphql');
+
+const server = require('../server.js');
+
+describe('GraphQL API', () => {
+  it.todo(`Returns { "data": { "info": "This is the API for Cody's Untitled Game." } }`);
+});
