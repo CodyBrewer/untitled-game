cc469657f570b1c08d579ca22bf9563c29717d79
CodyBrewer <codybrewerdev@gmail.com>
2020-05-23 11:35:30 -0600

    added comments

--- server/app.js
+++ server/app.js
@@ -6,9 +6,11 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+// Implement Third-Party Middleware
 app.use(helmet());
 app.use(cors());
 
+// Start app listening on PORT
 app.listen(PORT, () => {
   console.log(`Server running and listening on port: #${PORT}`);
 });
